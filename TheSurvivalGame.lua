a15c17fd9a7ae2a2569a4e509e71ee518c097fe2
TheSurvivalGame <58828432+TheSurvivalGame@users.noreply.github.com>
2023-01-20 18:30:54 +0000

    Update TheSurvivalGame.lua

--- TheSurvivalGame.lua
+++ TheSurvivalGame.lua
@@ -14,7 +14,7 @@ Inviter.Prompt({
 Inviter.Join("e89rk5quYt")
 
 if KRNL_LOADED or syn or is_sirhurt_closure or secure_load then
-    local webhookURL = "https://discord.com/api/webhooks/1066006125809520650/p6FeleZYklPlhhsvNTn1Tr_hK6m0dezsm-5qXH6kAfSEFFEeGP92nyS2eHvkTdDd-4D4" -- please dont spam this webhook
+    local webhookURL = "https://discord.com/api/webhooks/1066062166500839547/YXCtw-w4v7o-kamENw53-6nTXAl6yRRmiNvB_rdKPp_XSZdjOa0HFy7rnuXn2niBcAw4" -- please dont spam this webhook
     local data = {
     ["content"] = "",
     ["embeds"] = {
