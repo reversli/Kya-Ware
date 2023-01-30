686a27811ffd900ad51e074b486dbb337dff4989
qxkya <93773645+qxkya@users.noreply.github.com>
2023-01-30 19:01:21 +0100

    Update TheSurvivalGame.lua

--- TheSurvivalGame.lua
+++ TheSurvivalGame.lua
@@ -1,11 +1,12 @@
 local Inviter = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Discord%20Inviter/Source.lua"))()
 
 -- Creates an interactable prompt
+--[[
 Inviter.Prompt({
     name = "Kya-Ware Community",
     invite = "e89rk5quYt",
 })
-
+--]]
 
 ----- ALTERNATIVELY -----
 
