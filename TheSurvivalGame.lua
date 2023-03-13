4b526851dd37715f51ff1f261330aca7f18c3a76
reversli <59169691+reversli@users.noreply.github.com>
2023-03-14 02:13:33 +0300

    Update TheSurvivalGame.lua

--- TheSurvivalGame.lua
+++ TheSurvivalGame.lua
@@ -1,4 +1,4 @@
 -- idk why but it only worked while executing it separately
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/qxkya/Kya-Ware/main/remotes%20thing.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/qxkya/Kya-Ware/main/idk.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/reversli/Kya-Ware/main/remotes%20thing.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/reversli/Kya-Ware/main/idk.lua"))()
