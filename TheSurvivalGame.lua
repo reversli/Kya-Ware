7ae1e25a84524832e2f73358b1bb862d63c7eb0d
reversli <59169691+reversli@users.noreply.github.com>
2023-03-14 02:12:30 +0300

    Update TheSurvivalGame.lua

--- TheSurvivalGame.lua
+++ TheSurvivalGame.lua
@@ -1,12 +1,3 @@
-local Inviter = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Discord%20Inviter/Source.lua"))()
-
-Inviter.Prompt({
-    name = "Kya-Ware Community",
-    invite = "kyaware",
-})
-
-Inviter.Join("kyaware")
-
 -- idk why but it only worked while executing it separately
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/qxkya/Kya-Ware/main/remotes%20thing.lua"))()
